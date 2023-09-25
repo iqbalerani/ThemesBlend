9c4d9541990f08241e62b1fe599ae50137173999
Iqbal Erani <iqbalerani@gmail.com>
2023-09-25 17:40:36 +0500

    1 commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <a href="">Home</a>
+        <a href="">Contact</a>
+        <a href="">Sell</a>
+      </nav>
+    </div>
+  );
+};
